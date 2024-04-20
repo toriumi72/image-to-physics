db879254c59c1c4f35b46d9582d9594dd23720e3
toriumi72 <toriumi415@gmail.com>
2024-04-20 16:31:46 +0900

    簡単なデモ機能だけできた

---
+++ src/app/about/page.tsx
@@ -0,0 +1,16 @@
+"use client"
+import React from "react";
+import {Button} from "@nextui-org/react";
+const About = () => {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>This is the about page.</p>
+      <Button color="primary">
+        Button
+      </Button>
+    </div>
+  )
+}
+
+export default About;
